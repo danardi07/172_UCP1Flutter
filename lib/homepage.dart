27a6c60f46900c3542a6b1198b3ba9358076b0be
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2025-04-29 23:49:06 +0700

    menambahkan navigasi di setiap menu  ( Data Piket, Data Pelanggan dan Barang Masuk/Keluar) ke page masing"

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -8,6 +8,7 @@ class HomePage extends StatelessWidget {
   final String email;
   const HomePage({super.key, required this.email});
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
